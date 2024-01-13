530ce2246e2b99981f1813a169452a0049981764
John Roper <johnroper100@gmail.com>
2024-01-13 08:23:51 -0600

    fix vue version

--- dashboard/header.php
+++ dashboard/header.php
@@ -19,7 +19,7 @@
     <script src="https://cdn.jsdelivr.net/npm/trumbowyg@2/dist/plugins/indent/trumbowyg.indent.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/trumbowyg@2/dist/plugins/pasteembed/trumbowyg.pasteembed.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/trumbowyg@2/dist/plugins/history/trumbowyg.history.min.js"></script>
-    <script src="https://unpkg.com/vue@next"></script>
+    <script src="https://unpkg.com/vue@latest"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue-trumbowyg@4"></script>
